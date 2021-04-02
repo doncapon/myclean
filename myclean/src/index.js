0a6df9ddadcf9056217964ab0436b1d257d6853f
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-04-02 14:23:22 +0300

    initial commit

--- myclean/src/index.js
+++ myclean/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/react-fontawesome';
+import "font-awesome/css/font-awesome.min.css"; 
+import "./assets/icon/custom-icons/css/custom-icons.css"; 
+import "./assets/icon/elegant-icons/style.css"; 
+import "./assets/css/normalize.css"; 
+import "./assets/css/style.css";   
+import "./assets/css/responsive.css";   
+ 
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
